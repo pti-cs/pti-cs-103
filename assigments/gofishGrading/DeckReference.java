9cd7d8c5aa143b8dcef2cd50d876c5d6d1ad8d5d
ariel <arielbruner@gmail.com>
2020-04-22 18:15:26 -0400

    Added reference implementations for Card and Deck, and the grading tests for Card.
    Changed implementation of Deck, which had the shuffling done at draw time and not initialization time (to fit with doc and the actual deck inspiration).

---
+++ assigments/gofishGrading/DeckReference.java
@@ -0,0 +1,63 @@
+package gofishGrading;
+
+import gofish.Card;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Random;
+
+public class DeckReference {
+
+    private ArrayList<Card> cards;
+    private Random rand = new Random();
+
+    public DeckReference() {
+        String[] SUITS = {
+                "C", "D", "H", "S"
+        };
+
+        String[] RANKS = {
+                "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                "J", "Q", "K", "A"
+        };
+
+        // initialize deck
+        int n = SUITS.length * RANKS.length;
+
+        this.cards = new ArrayList<Card>();
+
+        for (int i = 0; i < RANKS.length; i++) {
+            for (int j = 0; j < SUITS.length; j++) {
+                Card newCard = new Card(SUITS[j], RANKS[i]);
+                this.cards.add(newCard);
+            }
+        }
+        Collections.shuffle(this.cards);
+    }
+
+    public boolean isEmpty() {
+        return this.cards.size() == 0;
+    }
+
+    public int size() {
+        return this.cards.size();
+    }
+
+    public Card draw() {
+        if (this.isEmpty())
+            throw new IllegalStateException("No cards left to draw!");
+        return this.cards.remove(this.cards.size() - 1);
+    }
+
+    public Collection<Card> deal(int nCards) {
+        if (this.cards.size()<nCards)
+            throw new IllegalStateException("Not enough cards to deal!");
+        ArrayList<Card> dealtCards = new ArrayList<Card>();
+        for (int i = 0; i < nCards; i++) {
+            dealtCards.add(this.draw());
+        }
+        assert dealtCards.size() == nCards;
+        return dealtCards;
+    }
+}
